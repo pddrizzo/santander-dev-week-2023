35cef73550ce0474fb7449ba0b5e1f68325e8f7a
Patricia Ducci Rizzo <pddrizzo@gmail.com>
2023-10-18 22:54:43 -0300

    Criação das classes de dados

---
+++ src/main/java/edu/lab/spring/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package edu.lab.spring.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.lab.spring.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
